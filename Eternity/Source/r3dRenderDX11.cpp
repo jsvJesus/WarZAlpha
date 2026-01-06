220208dcaf7550580c12e7f5cc8cfd561764415b
jsvJesus <es3n1n.eng@gmail.com>
2026-01-06 20:39:25 +0200

    add d3dx11 back buffer

--- Eternity/Source/r3dRenderDX11.cpp
+++ Eternity/Source/r3dRenderDX11.cpp
@@ -7,6 +7,12 @@
 r3dDX11RenderLayer::r3dDX11RenderLayer()
 : initialized_( false )
 , hWindow_( NULL )
+, deviceContext_( NULL )
+, swapChain_( NULL )
+, backBufferRTV_( NULL )
+, depthStencilView_( NULL )
+, backBufferWidth_( 0 )
+, backBufferHeight_( 0 )
 {
 }
 
@@ -22,6 +28,8 @@ int r3dDX11RenderLayer::Init( HWND hWindow, int XRes, int YRes, int BPP, int Fla
 	R3D_UNUSED( Flags );
 
 	hWindow_ = hWindow;
+	backBufferWidth_ = XRes;
+	backBufferHeight_ = YRes;
 	initialized_ = true;
 	return TRUE;
 }
@@ -30,6 +38,12 @@ void r3dDX11RenderLayer::Close()
 {
 	initialized_ = false;
 	hWindow_ = NULL;
+	deviceContext_ = NULL;
+	swapChain_ = NULL;
+	backBufferRTV_ = NULL;
+	depthStencilView_ = NULL;
+	backBufferWidth_ = 0;
+	backBufferHeight_ = 0;
 }
 
 void r3dDX11RenderLayer::Resize( int XRes, int YRes, int BPP, int Flags )
@@ -38,11 +52,60 @@ void r3dDX11RenderLayer::Resize( int XRes, int YRes, int BPP, int Flags )
 	R3D_UNUSED( YRes );
 	R3D_UNUSED( BPP );
 	R3D_UNUSED( Flags );
+	
+	backBufferWidth_ = XRes;
+	backBufferHeight_ = YRes;
 }
 
 bool r3dDX11RenderLayer::IsInitialized() const
 {
 	return initialized_;
+}
+
+void r3dDX11RenderLayer::SetDeviceContext( ID3D11DeviceContext* context )
+{
+	deviceContext_ = context;
+}
+
+void r3dDX11RenderLayer::SetSwapChain( IDXGISwapChain* swapChain )
+{
+	swapChain_ = swapChain;
+}
+
+void r3dDX11RenderLayer::SetBackBufferViews( ID3D11RenderTargetView* renderTargetView, ID3D11DepthStencilView* depthStencilView )
+{
+	backBufferRTV_ = renderTargetView;
+	depthStencilView_ = depthStencilView;
+}
+
+ID3D11DeviceContext* r3dDX11RenderLayer::GetDeviceContext() const
+{
+	return deviceContext_;
+}
+
+IDXGISwapChain* r3dDX11RenderLayer::GetSwapChain() const
+{
+	return swapChain_;
+}
+
+ID3D11RenderTargetView* r3dDX11RenderLayer::GetBackBufferRTV() const
+{
+	return backBufferRTV_;
+}
+
+ID3D11DepthStencilView* r3dDX11RenderLayer::GetDepthStencilView() const
+{
+	return depthStencilView_;
+}
+
+int r3dDX11RenderLayer::GetBackBufferWidth() const
+{
+	return backBufferWidth_;
+}
+
+int r3dDX11RenderLayer::GetBackBufferHeight() const
+{
+	return backBufferHeight_;
 }
 
 #endif // defined(R3D_USE_DX11)
--- Eternity/Source/r3dRenderDX11.h
+++ Eternity/Source/r3dRenderDX11.h
@@ -4,6 +4,11 @@
 
 #include <windows.h>
 
+struct ID3D11DeviceContext;
+struct ID3D11RenderTargetView;
+struct ID3D11DepthStencilView;
+struct IDXGISwapChain;
+
 class r3dDX11RenderLayer
 {
 public:
@@ -14,10 +19,27 @@ public:
 	void Close();
 	void Resize( int XRes, int YRes, int BPP, int Flags );
 	bool IsInitialized() const;
+	
+	void SetDeviceContext( ID3D11DeviceContext* context );
+	void SetSwapChain( IDXGISwapChain* swapChain );
+	void SetBackBufferViews( ID3D11RenderTargetView* renderTargetView, ID3D11DepthStencilView* depthStencilView );
+	
+	ID3D11DeviceContext* GetDeviceContext() const;
+	IDXGISwapChain* GetSwapChain() const;
+	ID3D11RenderTargetView* GetBackBufferRTV() const;
+	ID3D11DepthStencilView* GetDepthStencilView() const;
+	int GetBackBufferWidth() const;
+	int GetBackBufferHeight() const;
 
 private:
 	bool initialized_;
 	HWND hWindow_;
+	ID3D11DeviceContext* deviceContext_;
+	IDXGISwapChain* swapChain_;
+	ID3D11RenderTargetView* backBufferRTV_;
+	ID3D11DepthStencilView* depthStencilView_;
+	int backBufferWidth_;
+	int backBufferHeight_;
 };
 
 #endif // defined(R3D_USE_DX11)
